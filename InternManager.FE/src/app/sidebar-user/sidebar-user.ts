b7bc608df97fa3a5b575abd6a40c8244c12d378a
NhatDanh27 <nhatgaq90@gmail.com>
2026-05-20 08:05:20 +0700

    Mobile

--- InternManager.FE/src/app/sidebar-user/sidebar-user.ts
+++ InternManager.FE/src/app/sidebar-user/sidebar-user.ts
@@ -7,4 +7,18 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidebar-user.html',
   styleUrl: './sidebar-user.css',
 })
-export class SidebarUser {}
+export class SidebarUser {
+  isSidebarOpen = false; // Mặc định là đóng
+
+  // Hàm bật/tắt sidebar
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  // Hàm tự động đóng sidebar sau khi người dùng bấm chọn một menu để chuyển trang
+  closeSidebarOnMobile() {
+    if (window.innerWidth <= 768) {
+      this.isSidebarOpen = false;
+    }
+  }
+}
--- InternManager.FE/src/app/sidebarAdmin/sidebar.ts
+++ InternManager.FE/src/app/sidebarAdmin/sidebar.ts
@@ -9,5 +9,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+isSidebarOpen = false; // Mặc định là đóng
 
+  // Hàm bật/tắt sidebar
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  // Hàm tự động đóng sidebar sau khi người dùng bấm chọn một menu để chuyển trang
+  closeSidebarOnMobile() {
+    if (window.innerWidth <= 768) {
+      this.isSidebarOpen = false;
+    }
+  }
 }
